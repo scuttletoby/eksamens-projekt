cbed87ee1e5823514d28e8d5c9448714bc1ff244
scuttletoby <tobyscuttle@gmail.com>
2023-11-24 09:57:35 +0100

    remove old events

--- app/components/sortEvents.js
+++ app/components/sortEvents.js
@@ -1,9 +1,16 @@
 
 /* Sorterer events efter dato */
 export default function SortEvents(events) {
+    const currentDate = new Date();
+
     events.forEach((event) => {
         events.eventdate = {Date: new Date(event.eventDate)}
     });
     events.sort((a, b) => b.date - a.date);
+
+    events.forEach((event) => {
+        const eventDate = new Date(event.eventDate);
+        return eventDate >= currentDate;
+    })
     return events;
 }
--- app/page.js
+++ app/page.js
@@ -21,6 +21,7 @@ export default function Home() {
       <Centrum />
       {/* <HvemErVi /> */}
       <FrontSwiper />
+      {/* <PanoramaSwiper /> */}
       <Footer />
       <ScrollToTop />
     </>
